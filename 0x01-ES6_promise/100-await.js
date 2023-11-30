b1bfef899716b15d4f9ad6838bf45b76ff80a64b
emmanueladdo <asememmanueladdo@gmail.com>
2023-11-30 16:53:13 +0000

    awaits

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,10 +3,10 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   try {
     const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-    
+
     return {
-      photo: photo,
-      user: user,
+      photo,
+      user,
     };
   } catch (error) {
     console.error('Error:', error.message);

e6b8964dd484fd150ebd544ea198e40d362726fc
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-01 17:36:23 +0000

    Intilize room

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,15 +1,10 @@
-// Import the ClassRoom class from 0-classroom.js
 import ClassRoom from './0-classroom';
 
-// Function to initialize ClassRoom objects
-function initializeRooms() {
-  // Create an array of ClassRoom objects with sizes 19, 20, and 34
-  const rooms = [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
+export default function initializeRooms() {
+  const room = [];
+  room.push(new ClassRoom(19));
+  room.push(new ClassRoom(20));
+  room.push(new ClassRoom(34));
 
-  // Return the array of ClassRoom objects
-  return rooms;
+  return room;
 }

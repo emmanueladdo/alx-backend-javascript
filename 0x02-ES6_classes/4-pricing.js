ced357db3957ff859eebdd59d1d037406dd108e3
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-02 00:07:16 +0000

    Class for pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,25 @@
+/// class for Pricing
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.Currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amountVal) {
+    this._amount = amountVal;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currencyVal) {
+    this._currency = currencyVal;
+  }
+}

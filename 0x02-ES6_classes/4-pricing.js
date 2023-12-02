ceb6fbaeb22e636d9f984f3180059436b64d4ee3
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-02 00:20:00 +0000

    Class for pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -22,4 +22,10 @@ export default class Pricing {
   set currency(currencyVal) {
     this._currency = currencyVal;
   }
+  displayFullPrice(){
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+  }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }

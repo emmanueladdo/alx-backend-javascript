fc013b130ace8ba68e51ec112dd2b6cf8823a809
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-05 23:52:51 +0000

    Contains funxtion that list students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+// Function List all student
+export default function getListStudents() {
+  const ListofStudent = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return ListofStudent;
+}

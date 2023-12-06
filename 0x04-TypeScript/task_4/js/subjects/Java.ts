bfc280a85f268932d1fe278f9fe07e5957100ee0
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-06 23:55:50 +0000

    task 4

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,22 @@
+namespace Subjects {
+    export interface Teacher {
+      firstName: string;
+      lastName: string;
+      experienceTeachingJava?: number;
+    }
+  
+    export class Java extends Subject {
+      getRequirements = (): string => {
+        return "Here is the list of requirements for Java";
+      };
+  
+      getAvailableTeacher(): string {
+        if (this._teacher && this._teacher.experienceTeachingJava && this._teacher.experienceTeachingJava > 0) {
+          return `Available Teacher: ${this._teacher.firstName}`;
+        } else {
+          return `No available teacher`;
+        }
+      }
+    }
+  }
+  

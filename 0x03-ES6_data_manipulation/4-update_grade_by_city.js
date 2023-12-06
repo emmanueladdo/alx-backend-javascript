8f07a7103b07b1df3fb948af5edcd6784fc0c1fd
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-06 00:28:28 +0000

    upates grade

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+// function that update student grade by city
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const studentsCity = students.filter((student) => student.location === city);
+  return studentsCity.map((student) => {
+    const filteredGrades = newGrades.filter((grade) => grade.studentId === student.id);
+    if (filteredGrades.length > 0) {
+      return {
+        ...student,
+        grade: filteredGrades[0].grade,
+      };
+    }
+    return {
+      ...student,
+      grade: 'N/A',
+    };
+  });
+}

ccba9cbc8349af890cd9a040cdb688abba7f69af
emmanueladdo <asememmanueladdo@gmail.com>
2024-01-30 23:53:10 +0000

    Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,11 +3,9 @@
  * @param{string} message - msg to display
  */
 
-
-function displayMessage(message){
-    console.log(message);
+function displayMessage(message) {
+  console.log(message);
 }
 
-
-//Export function
+// Export function
 module.exports = displayMessage;

fc39fe236d3b9f5fdb095ba3f05ddf0e16b8d485
emmanueladdo <asememmanueladdo@gmail.com>
2024-01-30 23:47:47 +0000

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,13 @@
+/**
+ * Displays a msg on STGOUT
+ * @param{string} message - msg to display
+ */
+
+
+function displayMessage(message){
+    console.log(message);
+}
+
+
+//Export function
+module.exports = displayMessage;

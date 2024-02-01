df9f9364cf652e0881e79dc5455f9b37f1038cc8
emmanueladdo <asememmanueladdo@gmail.com>
2024-02-01 22:50:22 +0000

    Basic Integration testing

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,13 @@
+//API Availabl
+const express = require('express');
+const app = express();
+
+const port = 7865;
+
+app.get('/', function (req, res) {
+  res.send('Welcome to the payment system');
+});
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});

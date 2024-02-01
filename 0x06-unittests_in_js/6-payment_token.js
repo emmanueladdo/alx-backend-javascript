c7257e30ac263c0b9d7dde8768c57ea673f56d77
emmanueladdo <asememmanueladdo@gmail.com>
2024-02-01 22:30:28 +0000

    test

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,11 @@
+//Script defines function
+function getPaymentTokenFromAPI (success) {
+    return new Promise(function (resolve) {
+      if (success) {
+        resolve({ data: 'Successful response from the API' });
+      }
+    });
+  }
+  
+  module.exports = getPaymentTokenFromAPI;
+  

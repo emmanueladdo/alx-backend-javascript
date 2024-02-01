b86202f23fa9e539a9c1added2e9350f5d3f5663
emmanueladdo <asememmanueladdo@gmail.com>
2024-02-01 23:26:24 +0000

    Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -18,15 +18,14 @@ app.get('/available_payments', (req, res) => {
   res.send({
     payment_methods: {
       credit_cards: true,
-      paypal: false
-    }
+      paypal: false,
+    },
   });
 });
 
 app.post('/login', (req, res) => {
   res.send(`Welcome ${req.body.userName}`);
 });
-
 
 app.listen(port, () => {
   console.log('API available on localhost port 7865');

fddb0b81618d1a69870a7127045efe588de0af24
emmanueladdo <asememmanueladdo@gmail.com>
2024-02-01 21:57:39 +0000

    payment

---
+++ 0x06-unittests_in_js/4-payment.js
@@ -0,0 +1,10 @@
+// Payment APi
+
+const Utils = require('./utils');
+
+const sendPaymentRequestToAPI = (totalAmount, totalShipping) => {
+  const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${sum}`);
+};
+
+module.exports = sendPaymentRequestToAPI;

4830424b88cd6c446fef9fb3e08dad13543a0b20
emmanueladdo <asememmanueladdo@gmail.com>
2024-02-01 21:22:37 +0000

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,80 +1,80 @@
 // functin takes 3 argument and return operational
 const chai = require('chai');
-const calculateNumber = require('./2-calcul.js');
+const calculateNumber = require('./2-calcul_chai.js');
 
 const { expect } = chai;
 
 describe('calculateNumber', () => {
   describe('sum', () => {
     it('should round and return the sum of two numbers 1 + 3 = 4', () => {
-      expect(calculateNumber('SUM', 1, 3), 4);
+      expect(calculateNumber('SUM', 1, 3)).to.equal(4);
     });
     it('should round and return the sum of two numbers 1 + 3.7 = 5', () => {
-      expect(calculateNumber('SUM', 1, 3.7), 5);
+      expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
     });
     it('should round and return the sum of two numbers 1.2 + 3.7 = 5', () => {
-      expect(calculateNumber('SUM', 1.2, 3.7), 5);
+      expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
     });
     it('should round and return the sum of two numbers 1.5 + 3.7 = 6', () => {
-      expect(calculateNumber('SUM', 1.5, 3.7), 6);
+      expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
     });
     it('should round and return the sum of two numbers -2 + 4 = 2', () => {
-      expect(calculateNumber('SUM', -2, 4), 2);
+      expect(calculateNumber('SUM', -2, 4)).to.equal(2);
     });
     it('should round and return the sum of two numbers 2.6 + -4.7 = -2', () => {
-      expect(calculateNumber('SUM', 2.6, -4.7), -2);
+      expect(calculateNumber('SUM', 2.6, -4.7)).to.equal(-2);
     });
     it('should round and return the sum of two numbers -2.5 + -7.5 = -9', () => {
-      expect(calculateNumber('SUM', -2.5, -7.5), -9);
+      expect(calculateNumber('SUM', -2.5, -7.5)).to.equal(-9);
     });
   });
   describe('subtract', () => {
     it('should round and return the sum of two numbers 3 - 1 = 2', () => {
-      expect(calculateNumber('SUBTRACT', 3, 1), 2);
+      expect(calculateNumber('SUBTRACT', 3, 1)).to.equal(2);
     });
     it('should round and return the sum of two numbers 1 - 3.7 = -3', () => {
-      expect(calculateNumber('SUBTRACT', 1, 3.7), -3);
+      expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
     });
     it('should round and return the sum of two numbers 1.2 - 3.7 = -3', () => {
-      expect(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
+      expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-3);
     });
     it('should round and return the sum of two numbers 1.5 - 3.7 = -2', () => {
-      expect(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
+      expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.equal(-2);
     });
     it('should round and return the sum of two numbers -2 - 4 = -6', () => {
-      expect(calculateNumber('SUBTRACT', -2, 4), -6);
+      expect(calculateNumber('SUBTRACT', -2, 4)).to.equal(-6);
     });
     it('should round and return the sum of two numbers 2.6 - -4.7 = 8', () => {
-      expect(calculateNumber('SUBTRACT', 2.6, -4.7), 8);
+      expect(calculateNumber('SUBTRACT', 2.6, -4.7)).to.equal(8);
     });
     it('should round and return the sum of two numbers -2.5 - -7.5 = -9', () => {
-      expect(calculateNumber('SUBTRACT', -2.5, -7.5), 5);
+      expect(calculateNumber('SUBTRACT', -2.5, -7.5)).to.equal(5);
     });
   });
   describe('divide', () => {
     it('should round and return the sum of two numbers 3 / 1 = 3', () => {
-      expect(calculateNumber('DIVIDE', 3, 1), 3);
+      expect(calculateNumber('DIVIDE', 3, 1)).to.equal(3);
     });
     it('should round and return the sum of two numbers 1 / 3.7 = 0.25', () => {
-      expect(calculateNumber('DIVIDE', 1, 3.7), 0.25);
+      expect(calculateNumber('DIVIDE', 1, 3.7)).to.equal(0.25);
     });
     it('should round and return the sum of two numbers 1.2 / 3.7 = 0.25', () => {
-      expect(calculateNumber('DIVIDE', 1.2, 3.7), 0.25);
+      expect(calculateNumber('DIVIDE', 1.2, 3.7)).to.equal(0.25);
     });
     it('should round and return the sum of two numbers 1.5 / 3.7 = 0.5', () => {
-      expect(calculateNumber('DIVIDE', 1.5, 3.7), 0.5);
+      expect(calculateNumber('DIVIDE', 1.5, 3.7)).to.equal(0.5);
     });
     it('should round and return the sum of two numbers -2 / 4 = -0.5', () => {
-      expect(calculateNumber('DIVIDE', -2, 4), -0.5);
+      expect(calculateNumber('DIVIDE', -2, 4)).to.equal(-0.5);
     });
     it('should round and return the sum of two numbers 2.6 / -4.7 = -0.6', () => {
-      expect(calculateNumber('DIVIDE', 2.6, -4.7), -0.6);
+      expect(calculateNumber('DIVIDE', 2.6, -4.7)).to.equal(-0.6);
     });
     it('should round and return the sum of two numbers -2.5 / -5 = 0.4', () => {
-      expect(calculateNumber('DIVIDE', -2.5, -5), 0.4);
+      expect(calculateNumber('DIVIDE', -2.5, -5)).to.equal(0.4);
     });
     it('should return Error 1.4 / 0 = Error', () => {
-      expect(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
   });
 });
